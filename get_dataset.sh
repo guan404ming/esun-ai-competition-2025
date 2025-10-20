4dc3443b4165a485d4b6342d09d8d3d40a576078
Guan Ming(Wesley) Chiu <guanmingchiu@gmail.com>
2025-10-20 12:28:15 +0800

    Init env

---
+++ get_dataset.sh
@@ -0,0 +1,2 @@
+# Download dataset
+gdown 1XHJRi65ISZi4OPF_SbxQaxVsXFc7D24v -O data.zip
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from esun-ai-competition-2025!")
+
+
+if __name__ == "__main__":
+    main()
