9fb3271a33d1383afc7847117b7da89e035773d8
Guan Ming(Wesley) Chiu <guanmingchiu@gmail.com>
2025-10-20 12:42:01 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,2 +1,8 @@
 # Download dataset
-gdown 1XHJRi65ISZi4OPF_SbxQaxVsXFc7D24v -O data.zip
+gdown https://drive.google.com/drive/folders/1XHJRi65ISZi4OPF_SbxQaxVsXFc7D24v?usp=drive_link -O data --folder
+
+# unzip the dataset and rename to data
+unzip -o -O UTF-8 "data/40_初賽資料_V3 1.zip" -d data
+mv data/初賽資料/*.csv data/
+rm -rf data/初賽資料
+rm "data/40_初賽資料_V3 1.zip"
